c6f69ecbc2b1d5e18f238786acfb7d7e5cc1624e
ElmansouriAMINE <amineelmansouri2001@gmail.com>
2023-12-21 16:47:25 +0100

    update

--- apache_beam_script.py
+++ apache_beam_script.py
@@ -7,19 +7,17 @@ from hdfs import InsecureClient
 from apache_beam.io.filesystems import FileSystems
 from apache_beam.io.hadoopfilesystem import HadoopFileSystemOptions, HadoopFileSystem
 
-# Define the Parquet schema using pyarrow schema
+
 article_schema = schema([
-    ('title', 'utf8'),
     ('abstract', 'utf8'),
-    ('item_type', 'utf8'),
+    ('snippet', 'utf8'),
+    ('lead_paragraph', 'utf8'),
     ('source', 'utf8'),
-    ('updated_date', 'utf8'),
-    ('created_date', 'utf8'),
-    ('published_date', 'utf8'),
-    ('first_published_date', 'utf8'),
-    ('material_type_facet', 'utf8'),
-    ('kicker', 'utf8'),
-    ('subheadline', 'utf8')
+    ('pub_date', 'utf8'),
+    ('document_type', 'utf8'),
+    ('section_name', 'utf8'),
+    ('type_of_material', 'utf8'),
+    ('uri', 'utf8')
 ])
 
 # Define schema for Yahoo Finance data
@@ -29,17 +27,22 @@ yahoo_finance_schema = schema([
     ('High', 'double'),
     ('Low', 'double'),
     ('Close', 'double'),
-    ('Adj Close', 'double'),
     ('Volume', 'double')
 ])
 
+
+
 class NYTAPIClient(beam.DoFn):
     def process(self, element):
-        api_url = "https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=47Ecm6QPRMxjyIz8EmyD6g2agAiXMTKJ"
+        api_url = "https://api.nytimes.com/svc/archive/v1/2023/12.json?api-key=WEJG5L43BIt4E86fYVgWAAsvgeeeUV54"
         response = requests.get(api_url)
         api_response = response.json()
-        if 'results' in api_response:
-            yield api_response['results']
+
+        if 'response' in api_response and 'docs' in api_response['response']:
+            articles = api_response['response']['docs']
+            # for article in articles:
+            yield articles
+
 
 class YahooFinanceAPIClient(beam.DoFn):
     def process(self, element):
@@ -49,37 +52,38 @@ class YahooFinanceAPIClient(beam.DoFn):
             record_dict['Date'] = date.strftime('%Y-%m-%d')
             yield record_dict
 
+
+            
 def process_article(articles):
     selected_fields_list = []
     for article in articles:
         selected_fields = {
-            'title': article.get('title', ''),
             'abstract': article.get('abstract', ''),
-            'item_type': article.get('item_type', ''),
+            'snippet': article.get('snippet', ''),
+            'lead_paragraph': article.get('lead_paragraph', ''),
             'source': article.get('source', ''),
-            'updated_date': article.get('updated_date', ''),
-            'created_date': article.get('created_date', ''),
-            'published_date': article.get('published_date', ''),
-            'first_published_date': article.get('first_published_date', ''),
-            'material_type_facet': article.get('material_type_facet', ''),
-            'kicker': article.get('kicker', ''),
-            'subheadline': article.get('subheadline', '')
+            'pub_date': article.get('pub_date', ''),
+            'document_type': article.get('document_type', ''),
+            'section_name': article.get('section_name', ''),
+            'type_of_material': article.get('type_of_material', ''),
+            'uri': article.get('uri', ''),
+            
         }
         selected_fields_list.append(selected_fields)
 
     return selected_fields_list
 
 # Output paths in HDFS
-output_path_nyt = "./test2/nyt"
-output_path_yahoo = "./test2/yh"
+output_path_nyt = "./test6/nyt"
+output_path_yahoo = "./test6/yh"
 # Apache Beam Pipeline Options
 options = PipelineOptions()
 options.view_as(beam.options.pipeline_options.StandardOptions).runner = 'DirectRunner'
 
 # HDFS Client Setup
-hdfs_namenode_host = 'localhost'  # Replace with the actual hostname or IP address of your NameNode
-hdfs_namenode_port = 9870  # Port du NameNode
-hdfs_client = InsecureClient(f'http://{hdfs_namenode_host}:{hdfs_namenode_port}')
+# hdfs_namenode_host = 'localhost'  # Replace with the actual hostname or IP address of your NameNode
+# hdfs_namenode_port = 9870  # Port du NameNode
+# hdfs_client = InsecureClient(f'http://{hdfs_namenode_host}:{hdfs_namenode_port}')
 
 with beam.Pipeline(options=options) as pipeline:
     nyt_data = (
@@ -126,3 +130,58 @@ with beam.Pipeline(options=options) as pipeline:
 
 # print("Notre Beam Pipeline a été exécuté avec succès.")
 
+
+# Define the Parquet schema using pyarrow schema
+# article_schema = schema([
+#     ('title', 'utf8'),
+#     ('abstract', 'utf8'),
+#     ('item_type', 'utf8'),
+#     ('source', 'utf8'),
+#     ('updated_date', 'utf8'),
+#     ('created_date', 'utf8'),
+#     ('published_date', 'utf8'),
+#     ('first_published_date', 'utf8'),
+#     ('material_type_facet', 'utf8'),
+#     ('kicker', 'utf8'),
+#     ('subheadline', 'utf8')
+# ])
+    
+
+# class NYTAPIClient(beam.DoFn):
+#     def process(self, element):
+#         api_url = "https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=47Ecm6QPRMxjyIz8EmyD6g2agAiXMTKJ"
+#         response = requests.get(api_url)
+#         api_response = response.json()
+#         if 'results' in api_response:
+#             yield api_response['results']
+
+# class NYTAPIClient(beam.DoFn):
+#     def process(self, element):
+#         api_url = "https://api.nytimes.com/svc/archive/v1/2023/12.json?api-key=WEJG5L43BIt4E86fYVgWAAsvgeeeUV54"
+#         response = requests.get(api_url)
+#         api_response = response.json()
+        
+#         if 'response' in api_response and 'docs' in api_response['response']:
+#             articles = api_response['response']['docs']
+#             for article in articles:
+#                 yield article
+    
+# def process_article(articles):
+#     selected_fields_list = []
+#     for article in articles:
+#         selected_fields = {
+#             'title': article.get('title', ''),
+#             'abstract': article.get('abstract', ''),
+#             'item_type': article.get('item_type', ''),
+#             'source': article.get('source', ''),
+#             'updated_date': article.get('updated_date', ''),
+#             'created_date': article.get('created_date', ''),
+#             'published_date': article.get('published_date', ''),
+#             'first_published_date': article.get('first_published_date', ''),
+#             'material_type_facet': article.get('material_type_facet', ''),
+#             'kicker': article.get('kicker', ''),
+#             'subheadline': article.get('subheadline', '')
+#         }
+#         selected_fields_list.append(selected_fields)
+
+#     return selected_fields_list
